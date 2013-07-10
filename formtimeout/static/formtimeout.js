037450ff49be9ae00f047ddc3089cd0260b69d28
Balazs Ree <ree@greenfinity.hu>
2013-07-10 13:27:37 +0200

    add the actual javascript

--- formtimeout/static/formtimeout.js
+++ formtimeout/static/formtimeout.js
@@ -1,7 +1,28 @@
 
 jQuery(function () {
 
-    console.log('Running');
-
+    $('body').on('submit', 'form', function(evt) {
+        var $form = $(this);
+        var url = $form.attr('action');
+        var parms = $form.serialize();
+        console.log('submitted', url, parms);
+        $.ajax(url, {
+            method: 'POST',
+            data: parms,
+            format: 'html',
+            timeout: 3000
+        }).fail(function (xhr, textStatus) {
+            if (textStatus == 'timeout') {
+                alert('ERROR: The form submission has timed out.');
+            } else {
+                alert('ERROR: The form submission has failed. [' + textStatus + ']');
+            }
+        }).success(function(html) {
+            var newDoc = document.open("text/html", "replace");
+            newDoc.write(html);
+            newDoc.close();
+        });
+        return false;
+    });
 
 });
